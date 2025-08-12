e892074a3ebefe2fca7a7e2e8bcc6c28498edbcc
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-08-12 20:39:45 +0100

    add AuthController

---
+++ back/src/main/java/com/raxrot/back/controllers/AuthController.java
@@ -0,0 +1,95 @@
+package com.raxrot.back.controllers;
+
+import com.raxrot.back.enums.UserRole;
+import com.raxrot.back.exceptions.ApiException;
+import com.raxrot.back.models.User;
+import com.raxrot.back.repository.UserRepository;
+import com.raxrot.back.security.dto.LoginRequest;
+import com.raxrot.back.security.dto.LoginResponse;
+import com.raxrot.back.security.dto.SignupRequest;
+import com.raxrot.back.security.jwt.JwtUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/public/auth")
+public class AuthController {
+    @Autowired
+    JwtUtils jwtUtils;
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    // Логин (оставляем твой код)
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticateUser(@RequestBody LoginRequest loginRequest) {
+        Authentication authentication;
+        try {
+            authentication = authenticationManager
+                    .authenticate(new UsernamePasswordAuthenticationToken(
+                            loginRequest.getUsername(),
+                            loginRequest.getPassword()
+                    ));
+        } catch (AuthenticationException exception) {
+            return ResponseEntity
+                    .status(HttpStatus.UNAUTHORIZED)
+                    .body(Map.of("message", "Bad credentials", "status", false));
+        }
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        String jwtToken = jwtUtils.generateTokenFromUsername(userDetails);
+
+        LoginResponse response = new LoginResponse(
+                userDetails.getUsername(),
+                userRepository.findByUsername(userDetails.getUsername()).get().getRole(),
+                jwtToken
+        );
+
+        return ResponseEntity.ok(response);
+    }
+
+    // Регистрация
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@RequestBody SignupRequest signUpRequest) {
+
+        if (userRepository.findByUsername(signUpRequest.getUsername()).isPresent()) {
+            throw new ApiException("Username is already taken",HttpStatus.BAD_REQUEST);
+        }
+
+        if (userRepository.findByEmail(signUpRequest.getEmail()).isPresent()) {
+            throw new ApiException("Email is already in use",HttpStatus.BAD_REQUEST);
+        }
+
+        User user = new User();
+        user.setUsername(signUpRequest.getUsername());
+        user.setEmail(signUpRequest.getEmail());
+        user.setPassword(passwordEncoder.encode(signUpRequest.getPassword()));
+        user.setRole(UserRole.ROLE_USER);
+
+        userRepository.save(user);
+
+        return ResponseEntity.ok(Map.of("message", "User registered successfully!"));
+    }
+}
