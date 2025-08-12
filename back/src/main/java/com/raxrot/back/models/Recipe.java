382d6950989948e88761d13bf66c20c6941aa4ac
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-08-12 19:14:04 +0100

    add Recipe

---
+++ back/src/main/java/com/raxrot/back/models/Recipe.java
@@ -0,0 +1,46 @@
+package com.raxrot.back.models;
+
+import com.raxrot.back.enums.FoodDifficulty;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tbl_recipes")
+public class Recipe {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String title;
+    @Lob
+    @Column(nullable = false)
+    private String description;
+    @Lob
+    @Column(nullable = false)
+    private String ingredients;
+    private String image;
+
+    @Enumerated(EnumType.STRING)
+    private FoodDifficulty difficulty;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User author;
+}
