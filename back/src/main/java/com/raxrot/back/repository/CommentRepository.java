789acfe071dd0132e775ed6103421a9a30329689
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-08-13 16:32:07 +0100

    fix CommentRepository

--- back/src/main/java/com/raxrot/back/repository/CommentRepository.java
+++ back/src/main/java/com/raxrot/back/repository/CommentRepository.java
@@ -1,13 +1,11 @@
 package com.raxrot.back.repository;
 
 import com.raxrot.back.models.Comment;
-import com.raxrot.back.models.Recipe;
-import com.raxrot.back.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    List<Comment> findByRecipe(Recipe recipe);
-    List<Comment> findByUser(User user);
+    List<Comment> findByRecipeId(Long recipeId);
+    List<Comment> findByUserId(Long userId);
 }
