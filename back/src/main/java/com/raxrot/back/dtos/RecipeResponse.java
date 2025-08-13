3bda53f03b50740c687266ae8d5ea79da1974bf0
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-08-13 15:01:13 +0100

    add RecipeResponse

---
+++ back/src/main/java/com/raxrot/back/dtos/RecipeResponse.java
@@ -0,0 +1,21 @@
+package com.raxrot.back.dtos;
+
+import com.raxrot.back.enums.FoodDifficulty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RecipeResponse {
+    private Long id;
+    private String title;
+    private String description;
+    private String ingredients;
+    private String image;
+    private FoodDifficulty difficulty;
+    private UserResponseDTO user;
+}
