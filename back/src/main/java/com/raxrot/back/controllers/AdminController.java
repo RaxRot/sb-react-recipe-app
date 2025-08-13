460cd205499133df0306463b8ec79949f9f80611
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-08-13 14:53:26 +0100

    add AdminController

--- back/src/main/java/com/raxrot/back/controllers/AdminController.java
+++ back/src/main/java/com/raxrot/back/controllers/AdminController.java
@@ -1,15 +1,36 @@
 package com.raxrot.back.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.raxrot.back.dtos.UserResponseDTO;
+import com.raxrot.back.services.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/admin")
 public class AdminController {
 
-    @GetMapping
-    public String admin() {
-        return "admin";
+    private final UserService userService;
+    public AdminController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<UserResponseDTO>>getUsers() {
+        List<UserResponseDTO>userResponseDTOS=userService.getAllUsers();
+        return ResponseEntity.ok().body(userResponseDTOS);
+    }
+
+    @GetMapping("/users/{userId}")
+    public ResponseEntity<UserResponseDTO> getUser(@PathVariable Long userId) {
+        UserResponseDTO userResponseDTO=userService.getUserById(userId);
+        return ResponseEntity.ok().body(userResponseDTO);
+    }
+
+    @DeleteMapping("/users/{userId}")
+    public ResponseEntity<Void>deleteUser(@PathVariable Long userId) {
+        userService.deleteUser(userId);
+        return ResponseEntity.noContent().build();
     }
 }
