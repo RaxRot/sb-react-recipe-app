98469e505992c50232a822162ad82ff558425ed7
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-08-13 16:25:10 +0100

    fix repo

--- back/src/main/java/com/raxrot/back/repository/CommentRepository.java
+++ back/src/main/java/com/raxrot/back/repository/CommentRepository.java
@@ -3,8 +3,6 @@ package com.raxrot.back.repository;
 import com.raxrot.back.models.Comment;
 import com.raxrot.back.models.Recipe;
 import com.raxrot.back.models.User;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -12,5 +10,4 @@ import java.util.List;
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findByRecipe(Recipe recipe);
     List<Comment> findByUser(User user);
-    Page<Comment> findByRecipe(Recipe recipe, Pageable pageable);
 }
